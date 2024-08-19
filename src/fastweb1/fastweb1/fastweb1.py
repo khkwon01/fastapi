ef421fd9f788e9609ea9eb7ba94fcd762f691394
khkwon01 <khkwon01@gmail.com>
2024-08-19 13:34:40 +0900

    code 추가

--- src/fastweb1/fastweb1/fastweb1.py
+++ src/fastweb1/fastweb1/fastweb1.py
@@ -1,6 +1,34 @@
-from fastapi import FastAPI, Body, Response
+from fastapi import FastAPI, Body, Response, HTTPException
+import mysql.connector
+import uvicorn
+
 
 app = FastAPI()
+
+host = '10.10.1.95'
+database = 'fastapi'
+username = 'admin'
+password = 'Welcome#1'
+
+
+@app.post("/students/")
+async def insert_student(name: str, age: int, grade: str):
+    try:
+        connection = mysql.connector.connect(host=host, database=database,user=username, password=password)
+    except mysql.connector.Error as error:
+        raise HTTPException(status_code=500, detail=f"Database connection error: {error}")
+
+    try:
+        cursor = connection.cursor()
+        # Insert data into the students table
+        insert_query = "INSERT INTO students (name, age, grade) VALUES (%s, %s, %s)"
+        cursor.execute(insert_query, (name, age, grade))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return {"message": "Student data inserted successfully"}
+    except mysql.connector.Error as error:
+        raise HTTPException(status_code=500, detail=f"Database error: {error}")
 
 # uri path
 @app.get("/hi/{who}")
@@ -20,6 +48,10 @@ def greet(who:str = Body(embed=True)):
 def header(name: str, value: str, response:Response):
     response.headers[name] = value
     return "normal body"
+
+
+if __name__ == "__main__":
+    uvicorn.run("fastweb1:app")
 
 '''
 client test
